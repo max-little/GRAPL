fbd28af6dd9c5f68f130d59080979bf14fcc6e32
Max A. Little <102555976+max-little@users.noreply.github.com>
2023-11-30 00:43:16 +0000

    Add files via upload

--- grapl/test/unit_tests.py
+++ grapl/test/unit_tests.py
@@ -21,20 +21,22 @@ def run_all():
     test_non_identify2()
     test_complex_dag()
     test_markov()
+    test_fixall_greedy()
+    test_fixall_full()
 
     print('All unit tests passed')
 
 
 def test_cyclic():
     grapl_obj = dsl.GraplDSL()
-    grapl_filename = 'grapl/graphs/cyclic_graph.grapl'
+    grapl_filename = '/grapl/graphs/cyclic_graph.grapl'
     G = grapl_obj.readgrapl(open(grapl_filename, 'r').read())
     assert not G.isdag()
     assert not G.isayclic()
 
 def test_topsort():
     grapl_obj = dsl.GraplDSL()
-    grapl_filename = 'grapl/graphs/topsort_graph.grapl'
+    grapl_filename = '/grapl/graphs/topsort_graph.grapl'
     G = grapl_obj.readgrapl(open(grapl_filename, 'r').read())
     node_list = G.topsort()
     for node in node_list:
@@ -43,7 +45,7 @@ def test_topsort():
 
 def test_tianfactor():
     grapl_obj = dsl.GraplDSL()
-    grapl_filename = 'grapl/graphs/bareinboim_2020.grapl'
+    grapl_filename = '/grapl/graphs/bareinboim_2020.grapl'
     G = grapl_obj.readgrapl(open(grapl_filename, 'r').read())
     assert G.isayclic()
     assert all([(D in G.districts()) for D in [{'A'},{'F'},{'B','D'},{'E','C'}]])
@@ -52,7 +54,7 @@ def test_tianfactor():
 
 def test_fixing():
     grapl_obj = dsl.GraplDSL()
-    grapl_filename = 'grapl/graphs/richardson_2017.grapl'
+    grapl_filename = '/grapl/graphs/richardson_2017.grapl'
     G = grapl_obj.readgrapl(open(grapl_filename, 'r').read())
     id_expr, expr, isident = algs.idfixing(G, {'X2'}, {'X4'})
     assert not G.isdag()
@@ -61,7 +63,7 @@ def test_fixing():
 
 def test_identify1():
     grapl_obj = dsl.GraplDSL()
-    grapl_filename = 'grapl/graphs/shpitser_thesis1.grapl'
+    grapl_filename = '/grapl/graphs/shpitser_thesis1.grapl'
     G = grapl_obj.readgrapl(open(grapl_filename, 'r').read())
     id_expr, id_eqn, isident = algs.idfixing(G, {'X'}, {'Y1','Y2'})
     assert not G.isdag()
@@ -73,7 +75,7 @@ def test_identify1():
 
 def test_identify2():
     grapl_obj = dsl.GraplDSL()
-    grapl_filename = 'grapl/graphs/shpitser_thesis2.grapl'
+    grapl_filename = '/grapl/graphs/shpitser_thesis2.grapl'
     G = grapl_obj.readgrapl(open(grapl_filename, 'r').read())
     id_expr, id_eqn, isident = algs.idfixing(G, {'X'}, {'Y'})
     assert not G.isdag()
@@ -82,7 +84,7 @@ def test_identify2():
 
 def test_non_identify1():
     grapl_obj = dsl.GraplDSL()
-    grapl_filename = 'grapl/graphs/non_identify1.grapl'
+    grapl_filename = '/grapl/graphs/non_identify1.grapl'
     G = grapl_obj.readgrapl(open(grapl_filename, 'r').read())
     id_expr, id_eqn, isident = algs.idfixing(G, {'X'}, {'Y'})
     assert not G.isdag()
@@ -91,7 +93,7 @@ def test_non_identify1():
 
 def test_non_identify2():
     grapl_obj = dsl.GraplDSL()
-    grapl_filename = 'grapl/graphs/non_identify2.grapl'
+    grapl_filename = '/grapl/graphs/non_identify2.grapl'
     G = grapl_obj.readgrapl(open(grapl_filename, 'r').read())
     id_expr, id_eqn, isident = algs.idfixing(G, {'X'}, {'Y'})
     assert not G.isdag()
@@ -100,7 +102,7 @@ def test_non_identify2():
 
 def test_complex_dag():
     grapl_obj = dsl.GraplDSL()
-    grapl_filename = 'grapl/graphs/complex_dag.grapl'
+    grapl_filename = '/grapl/graphs/complex_dag.grapl'
     G = grapl_obj.readgrapl(open(grapl_filename, 'r').read())
     id_str, id_eqn, isdag = algs.truncfactor(G, {'X1'}, {'X3','X4','Y1'})
     assert all([(term in id_eqn.rhs.num) for term in [{'Y1','C','X1'},{'X3'},{'X4'},{'C'}]])
@@ -109,7 +111,7 @@ def test_complex_dag():
 
 def test_markov():
     grapl_obj = dsl.GraplDSL()
-    grapl_filename = 'grapl/graphs/markov1.grapl'
+    grapl_filename = '/grapl/graphs/markov1.grapl'
     G = grapl_obj.readgrapl(open(grapl_filename, 'r').read())
     cis, cisstr, isdag = algs.localmarkov(G)
     for ci in cis.ciset:
@@ -131,7 +133,7 @@ def test_markov():
 
 def test_dsep():
     grapl_obj = dsl.GraplDSL()
-    grapl_filename = 'grapl/graphs/markov1.grapl'
+    grapl_filename = '/grapl/graphs/markov1.grapl'
     G = grapl_obj.readgrapl(open(grapl_filename, 'r').read())
     isdsep, ci, cistr, isdag = algs.dseparate(G,{'D'},{'S'},{'G','I'})
     assert isdsep
@@ -140,4 +142,41 @@ def test_dsep():
     assert ci.Z == {'G','I'}
     isdsep, ci, cistr, isdag = algs.dseparate(G,{'D'},{'S'},{'G'})
     assert not isdsep
-
+    
+def test_fixall_greedy():
+    grapl_obj = dsl.GraplDSL()
+    grapl_filename = '/grapl/graphs/complex_dag.grapl'
+    G = grapl_obj.readgrapl(open(grapl_filename, 'r').read())
+    id_expr, expr, isident = algs.idfixall(G, {'X2'}, {'Y1','Y2'}, mode = "mostmrg", greedy = True)
+    assert G.isdag()
+    assert G.isayclic()
+    assert isident
+    
+    id_expr, expr, isident = algs.idfixall(G, {'X2'}, {'Y1','Y2'}, mode = "random")
+    assert G.isdag()
+    assert G.isayclic()
+    assert isident
+    
+    id_expr, expr, isident = algs.idfixall(G, {'X2'}, {'Y1','Y2'}, mode = "shortest", greedy = True)
+    assert G.isdag()
+    assert G.isayclic()
+    assert isident
+    
+def test_fixall_full():
+    grapl_obj = dsl.GraplDSL()
+    grapl_filename = '/grapl/graphs/general_scenario.grapl'
+    G = grapl_obj.readgrapl(open(grapl_filename, 'r').read())
+    id_expr, expr, isident = algs.idfixall(G, {'Y'}, {'X'}, mode = "all", greedy = False)
+    assert not G.isdag()
+    assert G.isayclic()
+    assert isident
+    
+    id_expr, expr, isident = algs.idfixall(G, {'Y'}, {'X'}, mode = "shortest", greedy = False)
+    assert not G.isdag()
+    assert G.isayclic()
+    assert isident
+    
+    id_expr, expr, isident = algs.idfixall(G, {'Y'}, {'X'}, mode = "mostmrg", greedy = False)
+    assert not G.isdag()
+    assert G.isayclic()
+    assert isident
