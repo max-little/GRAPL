40e8b861ce56e55ee4fa09c88c15c233d0ef302b
Max A. Little <102555976+max-little@users.noreply.github.com>
2023-11-30 00:42:03 +0000

    v1.6

--- grapl/algorithms.py
+++ grapl/algorithms.py
@@ -10,28 +10,25 @@ by acyclic directed mixed graphs (ADMGs).
 import grapl.expr as expr
 import grapl.eqn as eqn
 import grapl.condind as condind
+from grapl.fixtree import FixSeqTree
 import copy as cp
+import numpy as np
+from itertools import product
+import warnings
 
 
-def idfixing(G, X={}, Y={}):
-    """Implementation of Richardson et al.'s Theorem 60 for causal effect identification
-       from given ADMG. See: T.S. Richardson, J.M. Robins, I. Shpitser, 2012:
-       "Nested Markov properties for acyclic directed mixed graphs", UAI'12.
-
-       Parameters:
-       G (ADMG)           -- A mixed causal graph object
-       X (Set of Strings) -- Set of interventional variables, where each string is a random variable name (must not be empty)
-       Y (Set of Strings) -- Set of variables on which interventional expression is represented, where each string is a random
-                             variable name (if empty, then all variables in G except for those in X)
-
-       Returns: (String, Eqn, Boolean)
-       If interventional distribution is identifiable, returns the identified equation as a Latex
-       String, with the corresponding Eqn object, and True. Otherwise, returns '', None, False
+def preintvar(G, X = {}, Y = {}):
     """
-
-    # Must supply the set of interventional (cause) variables
-    if not X:
-        return '', None, False
+    Find Y*, the set of potential causes of effects Y, after taking into account interventions X in an ADMG G.
+    
+    Parameters:
+    G (ADMG)           -- A mixed causal graph object
+    X (Set of Strings) -- Set of interventional variables, where each string is a random variable name (must not be empty)
+    Y (Set of Strings) -- Set of variables on which interventional expression is represented, where each string is a random
+                          variable name.
+    Returns:
+    Y_star (Set of Strings) -- Set of potential causes of those effects after removing effect vars and the desired intervention vars
+    """
 
     # Collate variable names
     V = G.nodes()
@@ -40,82 +37,241 @@ def idfixing(G, X={}, Y={}):
     if not Y:
         Y = V.difference(X)
 
-    # Find Y* = an_G(V)_{V\A}(Y)
+    # Find Y* = an_G(V)_{V\A}(Y)    
     A = X
     VdA = V.difference(A)
     G_sub_VdA = G.sub(VdA)
     Y_star = G_sub_VdA.an(Y)
+    
+    return Y_star
 
-    # Find subgraph G(V)_Y* and its districts
-    G_sub_Ys = G.sub(Y_star)
-    D_y = G_sub_Ys.districts()
 
-    # For each district, determine post-fixed ADMG and corresponding expression
-    D_exprs = []
-    for D in D_y:
+def fixseqsdistr(G, D, X = {}, Y = {}, degrad = False):
+    """
+    Given a graph G, district D and desired effect and intervention variables, find all possible fixing sequences and
+    corresponding fixed graphs.
+    
+    Parameters:
+    G (ADMG)           -- A mixed causal graph object
+    D (Set of Strings) -- A district, where each string is a random variable name
+    X (Set of Strings) -- Set of interventional variables, where each string is a random variable name (must not be empty).
+    Y (Set of Strings) -- Set of variables on which interventional expression is represented, where each string is a random
+                          variable name.
+    degrad (Boolean)   -- If True, degrade the search tree to linear structure of searching. Default False.
+    
+    Returns:
+    fix_node_seqs (List of List of Strings) -- A list of all possible fixing sequences, where each sequence is a list of strings
+                                               representing the name of the node to be fixed. 
+    fix_graph_seqs (List of List of ADMG)   -- A list of all corresponding fixed graphs, where each graph is an ADMG object.
+    """
 
-        # Nodes to fix for this district, fixing transform: phi_{V\D}(p(V;G_V))
-        VdD = V.difference(D)
+    # Initialise fixing sequence searcher
+    seq_searcher = FixSeqTree(G = G, X = X, Y = Y, degrad = degrad)
 
-        # Initialize accumulated ADMG, G(V), and district expression, p(V)
-        G_fixed = cp.deepcopy(G)
-        D_expr = expr.Expr(num=[V])
-
-        # Try to apply fixing sequence
-        nodes_fix = VdD.copy()
-        while (len(nodes_fix) > 0):
-            
-            # Find a valid fixing node in sequence
-            fixable_nodes = {node for node in nodes_fix if G_fixed.isfixable(node)}
-            node_valid = (len(fixable_nodes) > 0)
-            if node_valid:
-                fix = fixable_nodes.pop()
-            else:
-                break
-            
-            # Fix or marginalize (Corollary 33) the accumulated expression
-            dispa_fix = G_fixed.dispa(fix)
-            fix_ch = G_fixed.ch({fix})
-            if len(fix_ch) == 0:
-                D_expr.fixmarginal(fix)
-            else:
-                D_expr.fix(fix, dispa_fix)
-
-            # Fix node (remove all its incoming edges) in accumulated ADMG,
-            # remove from remaining fixing sequence
-            G_fixed.fix(fix)
-            nodes_fix = nodes_fix.difference({fix})
-
-        if not node_valid:
-            break
-        
-        D_expr.simplify()
-        D_exprs.append(D_expr)
-
-    # At this point, if no valid fixing node, intervention is not identifiable
-    if not node_valid:
-        return '', None, False
+    # Find all possible fixing sequences and their fixed graphs
+    fix_node_seqs, fix_graph_seqs, identifiable = seq_searcher.searchdfixseq(D)
+    
+    if identifiable:
+        return fix_node_seqs, fix_graph_seqs, identifiable
     else:
+        return None, None, identifiable
+
+
+def exprfixseq(G, fix_node_seq, fix_graph_seq):
+    """
+    Given the graph G, a fixing node sequence and its corresponding the fixed graphs, compute a corresponding
+    distributional expression.
+    
+    Parameters:
+    G (ADMG)                       -- A mixed causal graph object
+    fix_node_seq (List of Strings) -- A list of strings representing the name of the node to be fixed.
+    fix_graph_seq (List of ADMG)   -- A list of corresponding fixed graphs, where each graph is an ADMG object.
+    
+    Returns:
+    fixexpr_str (String) -- The simplied expression as a string
+    fixexpr (Expr)       -- The expression as an Expr object
+    """
+
+    # Start with the joint distribution for all nodes
+    fixexpr = expr.Expr(num=[G.nodes()])
+    graph_seq = [G] + fix_graph_seq[:-1]
+    for i in range(len(fix_node_seq)):
+        fix = fix_node_seq[i]
+        G_fixed = graph_seq[i]
+
+        # Fix or marginalize (Corollary 33) the accumulated expression
+        # explain it more based on the paper
+        dispa_fix = G_fixed.dispa(fix)
+        fix_ch = G_fixed.ch({fix})
+
+        # if no children, then marginalize...
+        if len(fix_ch) == 0:
+            fixexpr.fixmarginal(fix)
+
+        # if there are children, then fix... 
+        else:
+            fixexpr.fix(fix, dispa_fix)
+
+    fixexpr.simplify()
+    fixexpr_str = fixexpr.tostr()
+    return fixexpr_str, fixexpr 
+
+
+def idfixall(G, X = {}, Y ={}, mode = "shortest", greedy = True):
+    """
+    Search for interventional probability expressions for the given causal graph
+    and retrieve results based on the selected mode.
+    A general implementation of Richardson et al.'s Theorem 60 for causal effect 
+    identification from given ADMG. See: T.S. Richardson, J.M. Robins, I. Shpitser, 2012:
+    "Nested Markov properties for acyclic directed mixed graphs", UAI'12.
+    
+    Parameters:
+    G (ADMG)           -- A mixed causal graph object
+    X (Set of Strings) -- Set of interventional variables, where each string is a random variable name (must not be empty)
+    Y (Set of Strings) -- Set of variables on which interventional expression is represented, where each string is a random
+                          variable name (if empty, then all variables in G except for those in X)
+    mode (String)      -- The mode for selecting interventional equations {'shortest', 'mostmrg', 'random', or 'all'}, default 'shortest'.
+    greedy (Boolean)   -- If True, use greedy strategy to select the shortest or most marginalized expression. Otherwise, global search applied. Default True.
+    
+    Returns:
+    Depending on the selected 'mode', it returns different results:
+    - 'shortest': a Latex string of identified equation with the least number of distributions, its corresponding Eqn object, tracking information of fixing, and a boolean indicating if the causal effect is identifiable.
+    - 'mostmrg': a Latex string of identified equation with the most number marginalized variables, its corresponding Eqn object, tracking information of fixing, and a boolean indicating if the causal effect is identifiable.
+    - 'random': A randomly selected Latex string of identified equation, its corresponding Eqn object, tracking information of fixing, and a boolean indicating if the causal effect is identifiable.
+    - 'all': A list of all Latex string of identified equations, a list of their corresponding Eqn objects, tracking information of fixing, and a boolean indicating if the causal effect is identifiable.
+    If the causal effect is not identifiable: returns '', None, None, False
+    """
+
+    if mode not in ["shortest", "mostmrg", "random", "all"]:
+        raise ValueError("'{}' is not a valid value for mode. Valid values are: 'shortest', 'mostmrg', 'random', 'all'. Default: 'shortest'. ".format(mode))
+    
+    # Find the set of potential causes of those effects after removing effect var and the desired intervention var
+    Y_star = preintvar(G, X, Y)
+
+    # Find the districts of G_Y*
+    G_sub_Ystar = G.sub(Y_star)
+    D_y = G_sub_Ystar.districts()
+    D_exprs = []
+    
+    for D in D_y:
+        # For each district, find fixing sequences and corresponding fixed graphs
+        fix_node_seqs, fix_graph_seqs, identifiable = fixseqsdistr(G = G, D = D, X = X, Y = Y, degrad = mode == "random")
+        if not identifiable:
+            return None, None, False
+        D_exprs.append([])
+
+        # According to the selected mode and greedy strategy, formulate the expression sequences
+        if mode == "shortest" and greedy:
+            shortest_dist_cnt = np.inf
+            shortest_D_expr = None
+            for i in range(len(fix_node_seqs)):
+                _, D_expr  = exprfixseq(G, fix_node_seqs[i], fix_graph_seqs[i])
+                D_num = D_expr.num
+                D_den = D_expr.den
+                dist_cnt = len(D_num) + len(D_den)
+                if dist_cnt < shortest_dist_cnt:
+                    shortest_dist_cnt = dist_cnt
+                    shortest_D_expr = D_expr
+            D_exprs[-1].append(shortest_D_expr)
+        elif mode == "mostmrg" and greedy:
+            most_mrg_cnt = -1
+            most_mrg_D_expr = None
+            for i in range(len(fix_node_seqs)):
+                _, D_expr  = exprfixseq(G, fix_node_seqs[i], fix_graph_seqs[i])
+                mrg_cnt = len(D_expr.mrg)
+                if mrg_cnt > most_mrg_cnt:
+                    most_mrg_cnt = mrg_cnt
+                    most_mrg_D_expr = D_expr
+            D_exprs[-1].append(most_mrg_D_expr)
+        else:
+            D_expr_num = []
+            D_expr_den = []
+            for i in range(len(fix_node_seqs)):
+                _, D_expr  = exprfixseq(G, fix_node_seqs[i], fix_graph_seqs[i])
+                D_num = D_expr.num
+                D_den = D_expr.den
+                if (D_num not in D_expr_num) and (D_den not in D_expr_den):
+                    D_exprs[-1].append(D_expr)
+                    D_expr_num.append(D_num)
+                    D_expr_den.append(D_den)
+    D_exprs_cartprod = list(product(*D_exprs))
+
+    id_str_all =[]
+    id_eqn_all = []
+
+    # Combining all expressions of fixing districts
+    for exprs in D_exprs_cartprod:
+        id_expr = expr.Expr()
+        id_expr.combine(exprs)
         Y_mrg = Y_star.difference(Y)
-        id_expr = expr.Expr(mrg=Y_mrg)
-        for ex in D_exprs:
-            clash_vars = id_expr.mrg.difference(id_expr.mrg.difference(ex.mrg))
-            for old_var in clash_vars:
-                new_var = old_var + chr(39)
-                ex.subsvar(old_var, new_var)
-            id_expr.num = id_expr.num + ex.num
-            id_expr.den = id_expr.den + ex.den
-            id_expr.mrg = id_expr.mrg.union(ex.mrg)
-
+        id_expr.mrg = id_expr.mrg.union(Y_mrg)
         id_expr.simplify()
-
-        # Construct Latex equation for the identified interventional distribution
         lhs = expr.Expr()
-        lhs.addvars(num=Y, dov=A)
+        lhs.addvars(num=Y, dov=X)
         id_eqn = eqn.Eqn(lhs, id_expr)
         id_str = id_eqn.tocondstr()
+        id_str_all.append(id_str)
+        id_eqn_all.append(id_eqn)
+    
+    # Modes for selecting identified equations. Options: Shortest, Most Marginalized, Random, All
+    if mode == "shortest":
+        shortest_dist_cnt = np.inf
+        shortest_id_eqn = None
+        shortest_id_str = None
+        for i,id_eqn in enumerate(id_eqn_all):
+            w_denom = id_eqn.rhs.den
+            w_nom = id_eqn.rhs.num
+            dist_cnt = len(w_denom) + len(w_nom)
+            if dist_cnt < shortest_dist_cnt:
+                shortest_dist_cnt = dist_cnt
+                shortest_id_eqn = id_eqn
+                shortest_id_str = id_str_all[i]
+        return shortest_id_str, shortest_id_eqn, identifiable
+    
+    elif mode == "mostmrg":
+        most_mrg_cnt = -1
+        most_mrg_id_eqn = None
+        most_mrg_id_str = None
+        for i, id_eqn in enumerate(id_eqn_all):
+            mrg_cnt = len(id_eqn.rhs.mrg)
+            if mrg_cnt > most_mrg_cnt:
+                most_mrg_cnt = mrg_cnt
+                most_mrg_id_eqn = id_eqn
+                most_mrg_id_str = id_str_all[i]
+        return most_mrg_id_str, most_mrg_id_eqn, identifiable
+    
+    elif mode == "random":
+        rd_id_eqn = id_eqn_all[0]
+        rd_id_str = id_str_all[0]
+        return rd_id_str, rd_id_eqn, identifiable
+    
+    elif mode == "all":
+        return id_str_all, id_eqn_all, identifiable
 
-        return id_str, id_eqn, True
+
+def idfixing(G, X={}, Y={}):
+    """A special case of the idfixall function which returns a randomly selected Latex string of 
+       identified equation, its corresponding Eqn object, and a boolean indicating if the causal effect is identifiable.
+        A general implementation of Richardson et al.'s Theorem 60 for causal effect identification from given ADMG.
+        See: T.S. Richardson, J.M. Robins, I. Shpitser, 2012: "Nested Markov properties for acyclic directed mixed graphs", UAI'12.
+
+       Parameters:
+       G (ADMG)           -- A mixed causal graph object
+       X (Set of Strings) -- Set of interventional variables, where each string is a random variable name (must not be empty)
+       Y (Set of Strings) -- Set of variables on which interventional expression is represented, where each string is a random
+                             variable name (if empty, then all variables in G except for those in X)
+
+       Returns: (String, Eqn, Boolean)
+       If interventional distribution is identifiable, returns a randomly selected identified 
+       equation as a Latex String, with the corresponding Eqn object, and True. Otherwise, 
+       returns '', None, False
+    """
+
+    id_str, id_eqn, identifiable = idfixall(G, X, Y, mode = "random")
+    if identifiable:
+        return id_str, id_eqn, identifiable
+    else:
+        return "", None, identifiable
 
 
 def dagfactor(G, Y={}, simplify=True):
@@ -160,7 +316,7 @@ def dagfactor(G, Y={}, simplify=True):
     dist_str = fac_eqn.tocondstr()
 
     return dist_str, fac_eqn, True
-    
+
 
 def truncfactor(G, X={}, Y={}, prefactor=True):
     """Truncated factorization ("g-formula") for DAGs (ADMGs with no bidirects).
---
+++ grapl/fixtree.py
@@ -0,0 +1,171 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 18 18:30:44 2023
+
+@author: NickMao
+"""
+import grapl.expr as expr
+import grapl.eqn as eqn
+import copy as cp
+import grapl.util as util
+from random import randint
+
+class FixNode:
+    """
+    A FixNode object to store the intermediate variables in the recursive tree growing and search process.
+    The member variables are: G_fixed (fixed graph), an ADMG object, the updated graph after a certain node is fixed;
+    D_expr (district expression), an Expr object, tracking the accumlated district expression; D_exprs (district
+    expressions), a List of Expr object, each Expr object is the district expression for a district; nodes_fix (nodes 
+    to be fixed), a List of Strings, each string being the name of the node that needs to be fixed; fixing_seq (fixing
+    sequence information), A String, recording the fixing sequence and process details; childern (child nodes), a List
+    of Node object, being the child nodes of a given Node.
+    """
+    def __init__(self, G_fixed, nodes_fix, fix_node_seq, fix_graph_seq):
+
+        self.G_fixed = G_fixed
+        self.nodes_fix = nodes_fix
+        self.fix_node_seq = fix_node_seq
+        self.fix_graph_seq = fix_graph_seq
+        self.if_dfixable = False
+        self.childern = []
+
+
+class FixSeqTree:
+    """
+    A Class object that generates a tree to search all possible fixing sequences for a district.
+    
+    Parameters:
+    G (ADMG)                    -- A mixed causal graph object
+    X (Set of Strings)          -- Set of interventional variables, where each string is a random variable name (must not be empty)
+    Y (Set of Strings)          -- Set of variables on which interventional expression is represented, where each string is a random
+                                   variable name (if empty, then all variables in G except for those in X)
+    """
+            
+    def __init__(self, G, X = {}, Y = {}, degrad = False):
+        self.G = G
+        self.X = X
+        self.Y = Y
+        self.degrad = degrad
+        
+        self.V = G.nodes()
+        self.A = X
+        
+        self.id_eqn_all = []
+        self.id_str_all = []
+        self.num_all = []
+        self.denum_all = []
+        
+        # Must supply the set of interventional (cause) variables
+        if not self.X:
+            print("Not idenfiable without interventional variable assigned!")
+            return
+        
+    def expand_tree_in_D(self, cur_node):
+        """
+        A recursive function for tree growing to search all possible 
+        fixing orders within a given district D.
+        
+        Parameters:
+        cur_node (FixNode): The current search node.
+        """
+
+        G_fixed = cur_node.G_fixed
+        nodes_fix = cur_node.nodes_fix
+        fix_node_seq = cur_node.fix_node_seq
+        fix_graph_seq = cur_node.fix_graph_seq
+        
+        if len(nodes_fix) > 0:
+
+            # Find a valid fixing node in sequence
+            fixable_nodes = [node for node in nodes_fix if G_fixed.isfixable(node)]
+            self.node_valid = (len(fixable_nodes) > 0)
+            if self.node_valid:
+
+                # Fix the graph by picking every possible fixable node
+                if not self.degrad:
+                    fixable_node_pool = [i for i in range(len(fixable_nodes))]
+                else:
+                    fixable_node_pool = [randint(0, len(fixable_nodes)-1)]
+                for fix_node_index in fixable_node_pool:             
+                    G_fixed_cp = cp.deepcopy(G_fixed)
+                    nodes_fix_cp = cp.deepcopy(nodes_fix)
+                    fix_node_seq_cp = cp.deepcopy(fix_node_seq)
+                    fix_graph_seq_cp = cp.deepcopy(fix_graph_seq)
+                    fix = fixable_nodes[fix_node_index]
+
+                    # Fix node (remove all its incoming edges) in accumulated ADMG
+                    G_fixed_cp.fix(fix)
+
+                    # Remove from remaining fixing sequence
+                    nodes_fix_cp = nodes_fix_cp.difference({fix})
+                    fix_node_seq_cp.append(fix)
+                    fix_graph_seq_cp.append(G_fixed_cp)
+                    child_node = FixNode(G_fixed = G_fixed_cp, nodes_fix = nodes_fix_cp, 
+                                         fix_node_seq = fix_node_seq_cp, fix_graph_seq = fix_graph_seq_cp)             
+                    cur_node.childern.append(child_node)
+            else:
+                return
+        else:
+            if self.node_valid:
+                cur_node.if_dfixable = True
+            else:
+                cur_node.if_dfixable = False
+            return
+        
+        # Expand the tree for every fixable node
+        for child in cur_node.childern:
+            self.expand_tree_in_D(child)
+        
+    def get_leaf(self, tree):
+        """
+        A recursive function to retrieve leaf nodes in the fixing tree structure.
+
+        Parameter:
+        tree (FixNode): The tree to search the leaf modes.
+        
+        Returns:
+        A List of leaf Nodes in the tree.
+        """
+
+        if len(tree.childern) == 0:
+            if tree.if_dfixable:
+                return [tree]
+            else:
+                return []
+        
+        leaf = []
+        for child in tree.childern:
+            leaf.extend(self.get_leaf(child))
+        
+        return leaf
+    
+    def searchdfixseq(self, D):
+        fix_node_seqs = []
+        fix_graph_seqs = []
+        
+        # Nodes to fix for this district, fixing transform: phi_{V\D}(p(V;G_V))
+        VdD = self.V.difference(D)
+        
+        # Initialize accumulated ADMG, G(V), and district expression, p(V)
+        G_fixed = cp.deepcopy(self.G)
+        nodes_fix = VdD.copy()
+        root_node = FixNode(G_fixed = G_fixed, nodes_fix = nodes_fix, fix_node_seq = [], fix_graph_seq = [])
+        valid_leaf = [root_node]
+        for i, leaf in enumerate(valid_leaf):
+            leaf.G_fixed = G_fixed
+            leaf.nodes_fix = nodes_fix
+            self.expand_tree_in_D(cur_node = leaf)
+            # retrieve leaf nodes for this district
+            if i == 0:
+                valid_leaf = self.get_leaf(leaf)
+            else:
+                valid_leaf.extend(self.get_leaf(leaf))
+        
+        for leaf in valid_leaf:
+            fix_node_seqs.append(leaf.fix_node_seq)
+            fix_graph_seqs.append(leaf.fix_graph_seq)
+            
+        if len(fix_node_seqs) > 0:
+            return fix_node_seqs, fix_graph_seqs, True
+        else:
+            return fix_node_seqs, fix_graph_seqs, False
